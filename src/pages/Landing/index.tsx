3894f0bef895be5bea987f150a763def59c3d5c0
Igor Bedesqui <igorbedesqui@gmail.com>
2020-08-03 21:53:07 -0300

    implemented Link from react-router-dom

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 import LogoImg from "../../assets/images/logo.svg";
 import LandingImg from "../../assets/images/landing.svg";
@@ -25,15 +26,15 @@ const Landing = () => {
         />
 
         <div className="buttons-container">
-          <a href="#a" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar" />
             Estudar
-          </a>
+          </Link>
 
-          <a href="#a" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas" />
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
